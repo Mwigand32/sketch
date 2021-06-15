84a2a1f8ca7bd0f2f3082989d32870a5ca510c1d
Mwigand32 <mwigand32@yahoo.com>
2021-06-15 12:17:54 -0700

    New Sketch Button added, grid columns need fix

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let gridSize = 16;
 
 const container = document.querySelector('#container');
+const startOver = document.querySelector('#newSketch');
 
 for (i = 1; i <= gridSize * gridSize; i++){
     const gridSquare = document.createElement('div');
@@ -9,7 +10,22 @@ for (i = 1; i <= gridSize * gridSize; i++){
     container.appendChild(gridSquare);
     gridSquare.addEventListener('mouseover', () => {
         gridSquare.style.cssText = 'background: black;';
-    })
-    
+    })   
 }
 
+startOver.addEventListener('click', () => {
+    for (i = 1; i <= gridSize * gridSize; i++){
+        container.removeChild(container.lastElementChild);
+    }
+    gridSize = prompt(`Enter size of your new sketchbox
+        (16-100)`);
+    for (i = 1; i <= gridSize * gridSize; i++){
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare-' + i)
+        
+        container.appendChild(gridSquare);
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.cssText = 'background: black;';
+        })   
+    }
+})
