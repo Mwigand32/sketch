519177ac5d824cff9c1de7053c677ac58a9b39f0
Mwigand32 <mwigand32@yahoo.com>
2021-06-15 11:30:49 -0700

    added 'hover' eventlistener

--- sketch.js
+++ sketch.js
@@ -4,9 +4,12 @@ const container = document.querySelector('#container');
 
 for (i = 1; i <= gridSize * gridSize; i++){
     const gridSquare = document.createElement('div');
-    gridSquare.classList.add('gridSquare-' + i);
+    gridSquare.classList.add('gridSquare-' + i)
     
     container.appendChild(gridSquare);
-
+    gridSquare.addEventListener('mouseover', () => {
+        gridSquare.style.cssText = 'background: black;';
+    })
     
 }
+
