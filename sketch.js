f6a559974c29241cd4ac098eafa65f999f56d4d4
Mwigand32 <mwigand32@yahoo.com>
2021-06-15 12:39:35 -0700

    reduced repetition with buildGrid function, grid columns need fix

--- sketch.js
+++ sketch.js
@@ -1,30 +1,11 @@
 let gridSize = 16;
-
+const defaultSize = 16;
 const container = document.querySelector('#container');
 const startOver = document.querySelector('#newSketch');
 
-for (i = 1; i <= gridSize * gridSize; i++){
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('gridSquare-' + i)
-    
-    container.appendChild(gridSquare);
-    gridSquare.addEventListener('mouseover', () => {
-        gridSquare.style.cssText = 'background: black;';
-    })   
-}
 
-startOver.addEventListener('click', () => {
-    for (i = 1; i <= gridSize * gridSize; i++){
-        container.removeChild(container.lastElementChild);
-    }
-    
-    do{
-    gridSize = prompt(`Enter size of your new sketchbox
-        (16-100)`);
-    }
-    while(gridSize < 16 || gridSize > 100 || gridSize == '')
-
-    for (i = 1; i <= gridSize * gridSize; i++){
+function buildGrid(newSize){
+    for (i = 1; i <= newSize * newSize; i++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('gridSquare-' + i)
         
@@ -33,4 +14,20 @@ startOver.addEventListener('click', () => {
             gridSquare.style.cssText = 'background: black;';
         })   
     }
+}
+
+startOver.addEventListener('click', () => {
+    for (i = 1; i <= gridSize * gridSize; i++){
+        container.removeChild(container.lastElementChild);
+    }
+
+    do{
+    gridSize = prompt(`Enter size of your new sketchbox:
+        (16-100)`);
+    }
+    while(gridSize < 16 || gridSize > 100 || gridSize == '')
+
+    buildGrid(gridSize);
 })
+
+buildGrid(defaultSize);
