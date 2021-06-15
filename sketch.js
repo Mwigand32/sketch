82e437a910c7103513207b7436ed80620522a9f8
Mwigand32 <mwigand32@yahoo.com>
2021-06-15 12:26:24 -0700

    added check for valid input on New Sketch prompt

--- sketch.js
+++ sketch.js
@@ -17,8 +17,13 @@ startOver.addEventListener('click', () => {
     for (i = 1; i <= gridSize * gridSize; i++){
         container.removeChild(container.lastElementChild);
     }
+    
+    do{
     gridSize = prompt(`Enter size of your new sketchbox
         (16-100)`);
+    }
+    while(gridSize < 16 || gridSize > 100 || gridSize == '')
+
     for (i = 1; i <= gridSize * gridSize; i++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('gridSquare-' + i)
